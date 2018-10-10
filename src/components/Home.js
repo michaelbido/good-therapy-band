36009166ae45b265b94de12d3adcffaddf1b6ecb
Michael Bido <michaelbido@gmail.com>
2018-10-09 19:05:28 -0500

    added hero banner to home, fixed css

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,23 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div>This is a test</div>
-        <p>Like us on Facebook!</p>
-        <div className="social-container">
-          <iframe id="facebook_iframe" className="facebook_iframe"></iframe>
+        <div className="flex-container">
+          <div className="band-name-container">
+            <h2>Good Therapy</h2>
+            <p>through soft classic rock</p>
+          </div>
+          <div className="social-container">
+            <p>Like us on &nbsp;
+              <a href="https://www.facebook.com/Goodtherapyband" rel="noopener noreferrer" target="_blank">Facebook</a>
+            </p>
+            <iframe id="facebook_iframe" className="facebook_iframe"></iframe>
+          </div>
+        </div>
+        <div className="hero-banner">
+          <div className="hero-content-container">
+            <p id="quote-text"> Keep on rockin' in the free world.</p>
+            <p id="quote-author">- Neil Young</p>
+          </div>
         </div>
       </div>
     )
