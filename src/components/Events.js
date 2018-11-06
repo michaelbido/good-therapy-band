1fa16a3e1c6c38cf6118035a0d8bb84b9eec752d
michaelbido <michaelbido@gmail.com>
2018-11-06 14:29:44 -0600

    CSS

--- src/components/Events.js
+++ src/components/Events.js
@@ -8,6 +8,9 @@ class Events extends Component {
 
   constructor(props) {
     super();
+    this.state = {
+      loadFbEvents: true,
+    }
   }
 
   singleEvent = (date, title, location, index) => {
@@ -21,8 +24,18 @@ class Events extends Component {
   }
 
   render() {
+
+    if (this.state.loadFbEvents) {
+      return (
+        <div className="events-container">
+          <h2>Events</h2>
+          <p>Like and Follow us on Facebook to keep track of our Events!</p>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fgoodtherapyband%2F&tabs=events&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=266877780635149" width="100%" height="500px" style={{border: 'none', overflow:'hidden'}} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+      )
+    }
     
-    if (this.props.data == null) {
+    else if (this.props.data == null) {
       return (
         <div className="events-container">
           <h2>Events</h2>
